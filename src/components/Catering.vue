29f88b04b2c08fe72e4f58a5756014c4062d6d0e
Worry-Bear21 <worryb807@gmail.com>
2026-05-21 10:58:39 +0800

    add again

--- src/components/Catering.vue
+++ src/components/Catering.vue
@@ -4,7 +4,24 @@
 
 <template>
     <div id="Catering">
-    <div>BREAKFAST</div>
-
+        <div>BREAKFAST</div>
+        <div class="container">
+            <div id="Break1">
+                <p id="ContextCat">Double Berry</p>
+                <button id="Order">Order now</button>
+            </div>
+            <div id="Break2">
+                <p id="ContextCat">Very Berry</p>
+                <button id="Order">Order now</button>
+            </div>
+            <div id="Break3">
+                <p id="ContextCat">Iced Berry Latte</p>
+                <button id="Order">Order now</button>
+            </div>
+            <div id="Break4">
+                <p id="ContextCat">Bonuts</p>
+                <button id="Order">Order now</button>
+            </div>
+        </div>
     </div>
 </template>

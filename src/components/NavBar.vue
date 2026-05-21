8c70b6ff30549669ae5684e3d9e0fdf9b9181d2b
Arnie <worryb807@gmail.com>
2026-05-22 07:35:29 +0800

    okay

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,46 +1,78 @@
 <script>
-
 export default {
-    data() {
-        return {
-            activePage: 'Menu'
-        }
-    },
-    methods: {
-        navigate(page) {
-            this.$emit('goTo', page);
-            this.activePage = page;
-        }
+  data() {
+    return {
+      activePage: 'Menu'
     }
+  },
+
+  methods: {
+    navigate(page) {
+      this.$emit('goTo', page)
+      this.activePage = page
+    }
+  }
 }
 </script>
 
 <template>
-    <nav id="NavBar">
-        <div id="NavCon">
-            <div id="NavMenu">
-                <h1 @click="navigate('Menu')" id="NavText">
-                    MENU</h1>
-                <h1 @click="navigate('Story')" id="NavText">
-                    OUR STORY</h1>
-                <h1 @click="navigate('Catering')" id="NavText">
-                    CATERING</h1>
-            </div>
-            <div>
-                <img id="Logo" src="../assets/images/maple.webp" alt="">
-            </div>
-            <div id="NavMenu" class="flex gap-5 mt-5">
-                <h1 id="NavText">
-                    GIFT CARDS</h1>
+  <nav id="NavBar">
 
-                <h1 id="NavText">
-                    SHOP</h1>
-                <h1 id="Btn">
-                    SIGN IN</h1>
+    <div id="NavCon">
 
-            </div>
-        </div>
+      <!-- LEFT -->
+      <div class="NavMenu">
 
-    </nav>
+        <h1
+          @click="navigate('Menu')"
+          :class="['NavText', activePage === 'Menu' ? 'active' : '']"
+        >
+          MENU
+        </h1>
 
+        <h1
+          @click="navigate('Story')"
+          :class="['NavText', activePage === 'Story' ? 'active' : '']"
+        >
+          OUR STORY
+        </h1>
+
+        <h1
+          @click="navigate('Catering')"
+          :class="['NavText', activePage === 'Catering' ? 'active' : '']"
+        >
+          CATERING
+        </h1>
+
+      </div>
+
+      <!-- LOGO -->
+      <div>
+        <img id="Logo" src="../assets/images/maple.webp" alt="">
+      </div>
+
+      <!-- RIGHT -->
+      <div class="NavMenu">
+
+        <h1 class="NavText">
+          GIFT CARDS
+        </h1>
+
+        <h1 class="ShopText">
+          SHOP
+        </h1>
+
+        <h1 class="NavText">
+          ORDER ONLINE
+        </h1>
+
+        <button id="Btn">
+          VISIT US
+        </button>
+
+      </div>
+
+    </div>
+
+  </nav>
 </template>
